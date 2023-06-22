3b07a6de3b31eb07d71f57b3fd3961cbe12a6f38
jashuakogila <120684954+jashuakogila@users.noreply.github.com>
2023-06-22 20:04:23 +0530

    my first commit

---
+++ src/main/java/com/domain/springboot/SpringbootCrudWebApp.java
@@ -0,0 +1,13 @@
+package com.domain.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudWebApp.class, args);
+	}
+
+}
